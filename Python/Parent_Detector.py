f1a086ec24f4e63ca2f7a2ea65e04f0d8b20433f
etolton49 <etolton49@charlottesvilleschools.org>
2019-10-04 11:40:39 -0400

    Final Assignments Push

---
+++ Python/Parent_Detector.py
@@ -0,0 +1,20 @@
+from picamera import PiCamera
+from time import sleep
+from gpiozero import MotionSensor
+
+camera = PiCamera()
+camera.rotation = 180
+
+
+
+pir = MotionSensor(4)
+
+while True:
+    pir.wait_for_motion()
+    camera.start_preview(fullscreen = False, window=(100,200,600,800))
+    camera.start_recording("/home/pi/Desktop/camera_photos/Intruder.h264")
+    print("move")
+    pir.wait_for_no_motion()
+    camera.stop_preview()
+    camera.stop_recording()
+    print(" ")
---
+++ Python/Stop_motion.py
@@ -0,0 +1,21 @@
+from time import sleep
+from picamera import PiCamera
+from gpiozero import Button
+
+button = Button(21                                                                                                                                                                                                                                            )
+camera = PiCamera()
+
+camera.rotation = 180
+camera.start_preview(fullscreen = False, window=(100,200,600,800))
+
+counter = 0    
+while True:
+    try:
+
+        button.wait_for_press()
+        camera.capture('/home/pi/Desktop/Animation/Button_capture%03d.jpg' % counter)
+        counter += 1
+
+    except KeyboardInterrupt:
+        camera.stop_preview()
+        break
